ecbfb58ced940039ac2ef388e5bad8b66c08908a
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-06 01:12:44 -0300

    🐛 fix: command background

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -25,10 +25,12 @@ interface CommandDialogProps extends DialogProps {}
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
     <Dialog {...props}>
-      <DialogContent className="overflow-hidden p-0 shadow-lg backdrop-blur-md dark:bg-transparent">
+      <DialogContent className="overflow-hidden p-0 shadow-lg dark:bg-transparent">
         <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
           {children}
         </Command>
+
+        <div className="dark:backdrop-blur-md absolute inset-0 pointer-events-none -z-10" />
       </DialogContent>
     </Dialog>
   );
