f053821d157ffa6a11096b86e762fb2e3e82f871
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-06 10:31:56 -0300

    🔥 route

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -1,19 +1,21 @@
-import { lazyFn, lazyRouteComponent } from "@tanstack/react-router";
-import { Route as IndexImport } from "./routes";
-import { Route as rootRoute } from "./routes/__root";
+/* eslint-disable prettier/prettier */
+import { lazyFn, lazyRouteComponent } from "@tanstack/react-router"
+
+import { Route as rootRoute } from "./routes/__root"
+import { Route as IndexImport } from "./routes"
 
 const IndexRoute = IndexImport.update({
   path: "/",
   getParentRoute: () => rootRoute,
-} as any);
+} as any)
 
 declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
     "/": {
-      preLoaderRoute: typeof IndexImport;
-      parentRoute: typeof rootRoute;
-    };
+      preLoaderRoute: typeof IndexImport
+      parentRoute: typeof rootRoute
+    }
   }
 }
 
-export const routeTree = rootRoute.addChildren([IndexRoute]);
+export const routeTree = rootRoute.addChildren([IndexRoute])
