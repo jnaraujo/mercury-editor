fbb0273bda6118b10a2c4a641f2b08c7ee56b077
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-08 20:42:19 -0300

    ♻️ refactor: extensions in editor

--- src/components/editor/extensions.ts
+++ src/components/editor/extensions.ts
@@ -2,11 +2,7 @@ import Placeholder from "@tiptap/extension-placeholder";
 import StarterKit from "@tiptap/starter-kit";
 
 export const extensions = [
-  StarterKit.configure({
-    heading: {
-      levels: [1, 2],
-    },
-  }),
+  StarterKit,
   Placeholder.configure({
     placeholder: `Clique em "/" para abrir o menu de comandos`,
     emptyNodeClass: `before:content-[attr(data-placeholder)] before:float-left before:h-0 before:pointer-events-none before:text-zinc-300 before:dark:text-zinc-600`,
