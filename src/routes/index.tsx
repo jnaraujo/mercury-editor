a4a1cec7232f6cf7b0936f6fe16974f0a0f57b1b
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-08 21:54:48 -0300

    ✨ feat: add empty notes message

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -41,6 +41,21 @@ export default function Home() {
                 onDelete={() => deleteNote(note.path)}
               />
             ))}
+
+            {sortedNotes.length === 0 && (
+              <div className="flex flex-col justify-center items-center gap-2 h-60">
+                <div className="flex flex-col justify-center items-center">
+                  <h2 className="text-zinc-700 dark:text-zinc-400 text-lg font-medium">
+                    Nenhuma nota encontrada
+                  </h2>
+                  <p className="text-zinc-600 dark:text-zinc-500 text-sm">
+                    Crie uma nota para começar
+                  </p>
+                </div>
+
+                <CreateNewNoteButton className="w-44" />
+              </div>
+            )}
           </ScrollArea>
         </section>
       </main>
