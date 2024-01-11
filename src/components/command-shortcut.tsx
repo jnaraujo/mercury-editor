d90f3447dd72f94d54829702cf4d21db50e7fc3b
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-10 22:24:00 -0300

    ♻️ refactor: CommandShortcut component to accept additional props

--- src/components/command-shortcut.tsx
+++ src/components/command-shortcut.tsx
@@ -1,10 +1,24 @@
-interface Props {
+import { cn } from "@/lib/utils";
+import { HtmlHTMLAttributes } from "react";
+
+interface Props extends HtmlHTMLAttributes<HTMLElement> {
   children: React.ReactNode;
+  className?: string;
 }
 
-export default function CommandShortcut({ children }: Props) {
+export default function CommandShortcut({
+  children,
+  className,
+  ...rest
+}: Props) {
   return (
-    <kbd className="pointer-events-none hidden h-4 w-fit select-none items-center gap-1 rounded border bg-muted px-1 font-mono text-[10px] font-medium opacity-100 sm:flex">
+    <kbd
+      className={cn(
+        "pointer-events-none hidden h-4 w-fit select-none items-center gap-1 rounded border bg-muted px-1 font-mono text-[10px] font-medium opacity-100 sm:flex",
+        className,
+      )}
+      {...rest}
+    >
       {children}
     </kbd>
   );
