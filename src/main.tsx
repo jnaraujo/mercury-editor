4f89d151976c4cf1bc5d4b91b40ef9c41dfcde1d
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-11 13:19:12 -0300

    ♻️ refactor: add startup time logging in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import ReactDOM from "react-dom/client";
 import "./styles.css";
 import { RouterProvider } from "react-router-dom";
 import { attachConsole } from "tauri-plugin-log-api";
-import { setupAppWindow } from "./lib/application";
+import { onStartupTimeEventReceived, setupAppWindow } from "./lib/application";
 import { router } from "./router";
 
 setupAppWindow().then(() => console.log("App window is ready"));
+onStartupTimeEventReceived().then(({ time }) => {
+  const elapsedTime = Date.now() - time;
+  console.log(`Startup time: ${elapsedTime}ms`);
+});
 
 attachConsole();
 
