078fc67b56369f00bb7a07dda92809306bfe4705
Jônatas Araújo <jonatasaraujo2257@gmail.com>
2024-01-12 21:39:12 -0300

    ✨ feat: update note button margin

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -75,7 +75,7 @@ export default function Note({
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <button
-            className="mr-1 flex items-start justify-center rounded-md p-2 outline-none ring-zinc-300 transition-opacity duration-200 focus:outline-none focus:ring-1 dark:ring-zinc-800"
+            className="mr-4 flex items-start justify-center rounded-md p-2 outline-none ring-zinc-300 transition-opacity duration-200 focus:outline-none focus:ring-1 dark:ring-zinc-800"
             aria-label="Apagar nota"
           >
             <MoreVertical
